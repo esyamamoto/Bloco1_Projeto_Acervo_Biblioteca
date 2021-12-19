4a51b2ffb9664290dd8d26a55cdfc10699562403
Erika Sayuri <erika.s.yamamoto@gmail.com>
2021-12-19 11:34:53 -0300

    Projeto I - Acervo Biblioteca

---
+++ src/com/generation/projeto_java/Exemplar.java
@@ -0,0 +1,29 @@
+package com.generation.projeto_java;
+
+public abstract class Exemplar {
+	String exemplar;
+	int numCadastro;
+	
+	
+	public Exemplar (String exemplar, int numCadastro) {
+		super();
+		this.exemplar = exemplar;
+		this.numCadastro = numCadastro;
+	}
+	public abstract void titulo();
+	public String getExemplar() {
+		return exemplar;
+	}
+	public void setExemplar(String exemplar) {
+		this.exemplar = exemplar;
+	}
+	public int getNumCadastro() {
+		return numCadastro;
+	}
+	public void setNumCadastro(int numCadastro) {
+		this.numCadastro = numCadastro;
+	}
+	public String toString() {
+		return this.exemplar + " | nº de série: " + this.numCadastro;			
+	}
+}	
